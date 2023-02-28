1bb4818c685a3508f024d93a0e9aa22961e958a9
JSYRD <1353097778@qq.com>
2023-02-28 14:32:59 +0800

    chore: delete useless comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-//
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//
-//
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-//               佛祖保佑         永无BUG
-//
-//
-//
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raspiopenthedoor/pages/homePage.dart';
 import 'package:raspiopenthedoor/utils/utils.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-// void main() {
-//   runApp(ChangeNotifierProvider<mainThemeData>.value(
-//       value: mainThemeData(), child: MyApp()));
-// }
+
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: ((context) => MainThemeData())),
@@ -58,22 +26,9 @@ class MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Raspi_FBI',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch:
               Provider.of<MainThemeData>(context, listen: true).getThemeColor),
       home: MyHomePage(title: 'Raspi_FBI'),
-      // routes: <String, WidgetBuilder>{
-      //   '/bottomNavigatorWidget': (context) =>
-      //       BottomNavigatorWidget(title: 'FBI')
-      // },
     );
   }
 }
